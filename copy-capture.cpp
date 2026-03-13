9785dc0a57d3a9cf5fccf53308ff0bd32819dce9
Scott Moreau <oreaus@gmail.com>
2026-03-13 09:00:18 -0600

    Composite children surfaces and cursors onto the base view

--- copy-capture.cpp
+++ copy-capture.cpp
@@ -27,12 +27,14 @@
 #include <wayfire/scene-render.hpp>
 #include <wayfire/output-layout.hpp>
 #include <wayfire/workspace-set.hpp>
+#include <wayfire/view-transform.hpp>
 #include <wayfire/scene-operations.hpp>
 #include <wayfire/signal-definitions.hpp>
 #include <wayfire/nonstd/wlroots-full.hpp>
 
 extern "C"
 {
+#include <wlr/types/wlr_xdg_shell.h>
 #include <wlr/types/wlr_ext_image_copy_capture_v1.h>
 #include <wlr/interfaces/wlr_ext_image_capture_source_v1.h>
 #include <drm_fourcc.h>
@@ -50,7 +52,7 @@ namespace copy_capture
 class copy_capture_plugin : public wf::plugin_interface_t
 {
     std::unique_ptr<wf::scene::render_instance_manager_t> instance_manager = nullptr;
-    std::map<wayfire_view, wlr_ext_foreign_toplevel_handle_v1 *> toplevels;
+    std::map<wayfire_view, wlr_ext_foreign_toplevel_handle_v1*> toplevels;
     wlr_ext_foreign_toplevel_handle_v1 *toplevel_handle;
     wf::wl_listener_wrapper on_new_request;
     wlr_swapchain *swapchain;
@@ -72,19 +74,16 @@ class copy_capture_plugin : public wf::plugin_interface_t
         {
             handle_new_request(data);
         });
-        on_new_request.connect(&wf::get_core().protocols.foreign_toplevel_image_capture_source->events.new_request);
-        for (auto& output : wf::get_core().output_layout->get_outputs())
+        on_new_request.connect(
+            &wf::get_core().protocols.foreign_toplevel_image_capture_source->events.new_request);
+        for (auto& view : wf::get_core().get_all_views())
         {
-            for (auto& view : output->wset()->get_views())
-            {
-                add_toplevel_view(view);
-            }
+            add_toplevel_view(view);
         }
     }
 
     scene::damage_callback push_damage = [=] (wf::region_t region)
-    {
-    };
+    {};
 
     void destroy_render_instance_manager()
     {
@@ -124,7 +123,7 @@ class copy_capture_plugin : public wf::plugin_interface_t
             return selected_view;
         }
 
-        for (auto &[view, toplevel] : toplevels)
+        for (auto & [view, toplevel] : toplevels)
         {
             if (toplevel == toplevel_handle)
             {
@@ -142,58 +141,122 @@ class copy_capture_plugin : public wf::plugin_interface_t
         {
             return;
         }
+
         auto root_node = view->get_surface_root_node();
-        const wf::geometry_t bbox = root_node->get_bounding_box();
+        auto bbox   = view->get_root_node()->get_children_bounding_box();
+        auto output = view->get_output();
+        if (!output)
+        {
+            return;
+        }
+
         wf::render_target_t target = wf::render_target_t(dst.get_renderbuffer());
+
+        bbox.width  = toplevel_source.width;
+        bbox.height = toplevel_source.height;
 
         target.geometry = bbox;
         target.scale    = 1.0f;
 
         std::vector<scene::render_instance_uptr> instances;
-        root_node->gen_render_instances(instances, [] (auto) {}, view->get_output());
-
         render_pass_params_t params;
         params.background_color = {0, 0, 0, 0};
         params.damage    = bbox;
         params.target    = target;
         params.instances = &instances;
         params.flags     = RPASS_CLEAR_BACKGROUND;
-        render_pass_t::run(params);
+        auto pass = render_pass_t{params};
+        pass.run_partial();
+
+        auto views = wf::get_core().get_all_views();
+        std::vector<wayfire_view> reversed_views_vector{views.rbegin(), views.rend()};
+        for (auto& candidate : reversed_views_vector)
+        {
+            auto candidate_root_node = candidate->get_surface_root_node();
+            if (!candidate->get_wlr_surface())
+            {
+                continue;
+            }
+
+            if (auto xdg_popup = wlr_xdg_popup_try_from_wlr_surface(candidate->get_wlr_surface()))
+            {
+                if (xdg_popup->parent == view->get_wlr_surface())
+                {
+                    candidate_root_node->gen_render_instances(instances, [] (auto) {}, view->get_output());
+                }
+            }
+        }
+
+        for (auto& candidate : wf::get_core().get_all_views())
+        {
+            auto candidate_root_node = candidate->get_surface_root_node();
+            if (!candidate->get_wlr_surface())
+            {
+                continue;
+            }
+
+            if (candidate_root_node->parent() == dynamic_cast<wf::scene::node_t*>(root_node.get()))
+            {
+                candidate_root_node->gen_render_instances(instances, [] (auto) {}, view->get_output());
+            }
+        }
+
+        auto children = view->get_root_node()->get_children();
+        std::vector<std::shared_ptr<wf::scene::node_t>> reversed_node_vector{children.rbegin(),
+            children.rend()};
+        for (auto & node : reversed_node_vector)
+        {
+            node->gen_render_instances(instances, [] (auto) {}, view->get_output());
+        }
+
+        params.flags = 0;
+        pass = render_pass_t{params};
+        pass.run_partial();
+
+        wlr_output_cursor *cursor;
+        wl_list_for_each(cursor, &output->handle->cursors, link)
+        {
+            wf::geometry_t geometry{int(cursor->x), int(cursor->y), int(cursor->width), int(cursor->height)};
+            pass.add_texture(wf::texture_t{cursor->texture}, target, geometry, wf::region_t{geometry}, 1.0);
+        }
+        pass.submit();
     }
 
     void handle_new_request(void *data)
-    {LOGI(__func__);
+    {
         wlr_ext_foreign_toplevel_image_capture_source_manager_v1_request *request =
-            (wlr_ext_foreign_toplevel_image_capture_source_manager_v1_request *) data;
+            (wlr_ext_foreign_toplevel_image_capture_source_manager_v1_request*)data;
         toplevel_handle = request->toplevel_handle;
-        selected_view = get_view();
+        selected_view   = get_view();
         if (!selected_view)
         {
             LOGI("Could not find selected view to satisfy copy capture request.");
             return;
         }
+
         ext_image_capture_source_v1_init(&toplevel_source);
         ext_image_capture_source_v1_cursor_init(&cursor_source);
         const wf::geometry_t bbox = selected_view->get_surface_root_node()->get_bounding_box();
-        toplevel_source.width = bbox.width;
+        toplevel_source.width  = bbox.width;
         toplevel_source.height = bbox.height;
         wlr_drm_format format
         {
             .format = DRM_FORMAT_ABGR8888,
-            .len = 0,
+            .len    = 0,
         };
         swapchain = wlr_swapchain_create(wf::get_core().allocator, bbox.width, bbox.height, &format);
         wlr_swapchain_slot slot{};
         dst.allocate({bbox.width, bbox.height});
         slot.buffer = dst.get_buffer();
         swapchain->slots[0] = slot;
-        wlr_ext_image_capture_source_v1_set_constraints_from_swapchain(&toplevel_source, swapchain, wf::get_core().renderer);
+        wlr_ext_image_capture_source_v1_set_constraints_from_swapchain(&toplevel_source, swapchain,
+            wf::get_core().renderer);
         wlr_ext_foreign_toplevel_image_capture_source_manager_v1_request_accept(request, &toplevel_source);
     }
 
     void add_toplevel_view(wayfire_view view)
     {
-        if (!view || view->role != wf::VIEW_ROLE_TOPLEVEL)
+        if (!view || (view->role != wf::VIEW_ROLE_TOPLEVEL))
         {
             return;
         }
@@ -204,12 +267,13 @@ class copy_capture_plugin : public wf::plugin_interface_t
             view->get_app_id().c_str(),
         };
 
-        toplevels[view] = wlr_ext_foreign_toplevel_handle_v1_create(wf::get_core().protocols.foreign_toplevel_list, &state);
+        toplevels[view] = wlr_ext_foreign_toplevel_handle_v1_create(
+            wf::get_core().protocols.foreign_toplevel_list, &state);
     }
 
     size_t remove_toplevel_view(wayfire_view view)
     {
-        if (!view || view->role != wf::VIEW_ROLE_TOPLEVEL)
+        if (!view || (view->role != wf::VIEW_ROLE_TOPLEVEL))
         {
             return 0;
         }
@@ -230,7 +294,7 @@ class copy_capture_plugin : public wf::plugin_interface_t
 
     wf::signal::connection_t<wf::view_unmapped_signal> on_view_unmapped = [=] (wf::view_unmapped_signal *ev)
     {
-        if (remove_toplevel_view(ev->view) > 0)
+        if ((remove_toplevel_view(ev->view) > 0) && (selected_view == ev->view))
         {
             deactivate();
         }
@@ -242,6 +306,7 @@ class copy_capture_plugin : public wf::plugin_interface_t
         {
             return;
         }
+
         destroy_render_instance_manager();
         selected_view = nullptr;
     }
@@ -252,7 +317,7 @@ class copy_capture_plugin : public wf::plugin_interface_t
         on_view_mapped.disconnect();
         on_view_unmapped.disconnect();
 
-        for (auto &[view, toplevel] : toplevels)
+        for (auto & [view, toplevel] : toplevels)
         {
             wlr_ext_foreign_toplevel_handle_v1_destroy(toplevel);
         }
@@ -268,11 +333,12 @@ wf::copy_capture::copy_capture_plugin *plugin;
 static void source_start(struct wlr_ext_image_capture_source_v1 *source, bool with_cursors)
 {
     LOGI(__func__, ": Client started toplevel copy capture.");
-    plugin = (wf::copy_capture::copy_capture_plugin *) plugin_ptr;
+    plugin = (wf::copy_capture::copy_capture_plugin*)plugin_ptr;
     if (!plugin)
     {
         return;
     }
+
     plugin->destroy_render_instance_manager();
     plugin->create_render_instance_manager();
     plugin->last_time = wf::get_current_time();
@@ -281,33 +347,36 @@ static void source_start(struct wlr_ext_image_capture_source_v1 *source, bool wi
 static void source_stop(struct wlr_ext_image_capture_source_v1 *source)
 {
     LOGI(__func__, ": Client stopped toplevel copy capture.");
-    plugin = (wf::copy_capture::copy_capture_plugin *) plugin_ptr;
+    plugin = (wf::copy_capture::copy_capture_plugin*)plugin_ptr;
     if (!plugin)
     {
         return;
     }
+
     plugin->deactivate();
 }
 
 static void source_request_frame(struct wlr_ext_image_capture_source_v1 *source,
-        bool schedule_frame)
+    bool schedule_frame)
 {
-    plugin = (wf::copy_capture::copy_capture_plugin *) plugin_ptr;
+    plugin = (wf::copy_capture::copy_capture_plugin*)plugin_ptr;
     if (!plugin)
     {
         return;
     }
+
     int64_t elapsed = wf::get_current_time() - plugin->last_time;
     while (elapsed < (1000 / plugin->target_fps))
     {
         wl_display_dispatch(wf::get_core().display);
         elapsed = wf::get_current_time() - plugin->last_time;
     }
+
     plugin->last_time += elapsed;
     plugin->view_snapshot();
     wf::region_t damage = wf::region_t{wf::geometry_t{0, 0,
-        int(plugin->toplevel_source.width),
-        int(plugin->toplevel_source.height)}};
+            int(plugin->toplevel_source.width),
+            int(plugin->toplevel_source.height)}};
     wlr_ext_image_capture_source_v1_frame_event frame_event
     {
         .damage = damage.to_pixman(),
@@ -316,49 +385,53 @@ static void source_request_frame(struct wlr_ext_image_capture_source_v1 *source,
 }
 
 static void source_copy_frame(struct wlr_ext_image_capture_source_v1 *source,
-        struct wlr_ext_image_copy_capture_frame_v1 *frame,
-        struct wlr_ext_image_capture_source_v1_frame_event *frame_event)
+    struct wlr_ext_image_copy_capture_frame_v1 *frame,
+    struct wlr_ext_image_capture_source_v1_frame_event *frame_event)
 {
-    plugin = (wf::copy_capture::copy_capture_plugin *) plugin_ptr;
+    plugin = (wf::copy_capture::copy_capture_plugin*)plugin_ptr;
     if (!plugin)
     {
         return;
     }
+
     auto buffer = plugin->dst.get_buffer();
     if (!wlr_ext_image_copy_capture_frame_v1_copy_buffer(frame, buffer,
         wf::get_core().renderer))
     {
         LOGI("Failed to copy view buffer to client frame buffer!");
-        if (buffer->width != frame->buffer->width || buffer->height != frame->buffer->height)
+        if ((buffer->width != frame->buffer->width) || (buffer->height != frame->buffer->height))
         {
             LOGI("The src and dst sizes differ: ",
                 buffer->width, "x", buffer->height, " : ",
                 frame->buffer->width, "x", frame->buffer->height);
         }
+
         plugin->deactivate();
         return;
     }
+
     timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
     wlr_ext_image_copy_capture_frame_v1_ready(frame, WL_OUTPUT_TRANSFORM_NORMAL, &ts);
 }
 
 struct wlr_ext_image_capture_source_v1_cursor *get_pointer_cursor(
-		struct wlr_ext_image_capture_source_v1 *source, struct wlr_seat *seat)
+    struct wlr_ext_image_capture_source_v1 *source, struct wlr_seat *seat)
 {
-    plugin = (wf::copy_capture::copy_capture_plugin *) plugin_ptr;
+    plugin = (wf::copy_capture::copy_capture_plugin*)plugin_ptr;
     if (!plugin)
     {
         return nullptr;
     }
+
     return &plugin->cursor_source;
 }
 
 static const struct wlr_ext_image_capture_source_v1_interface source_impl = {
     .start = source_start,
-    .stop = source_stop,
+    .stop  = source_stop,
     .request_frame = source_request_frame,
-    .copy_frame = source_copy_frame,
+    .copy_frame    = source_copy_frame,
     .get_pointer_cursor = get_pointer_cursor,
 };
 
@@ -372,13 +445,21 @@ void ext_image_capture_source_v1_cursor_init(wlr_ext_image_capture_source_v1_cur
     wlr_ext_image_capture_source_v1_cursor_init(source, &source_impl);
 }
 
-
 /* XXX: Hack: We need a copy of the class instance, so instead of using
  * the DECLARE_WAYFIRE_PLUGIN macro, we define our own. It shims in code
  * that provides us of a pointer to the class, whenever wayfire gets a
  * new instance. */
 extern "C"
 {
-    wf::plugin_interface_t*newInstance() { auto p = new wf::copy_capture::copy_capture_plugin; plugin_ptr = (void *) p; return p; }
-    uint32_t getWayfireVersion() { return WAYFIRE_API_ABI_VERSION; }
+    wf::plugin_interface_t *newInstance()
+    {
+        auto p = new wf::copy_capture::copy_capture_plugin;
+        plugin_ptr = (void*)p;
+        return p;
+    }
+
+    uint32_t getWayfireVersion()
+    {
+        return WAYFIRE_API_ABI_VERSION;
+    }
 }
